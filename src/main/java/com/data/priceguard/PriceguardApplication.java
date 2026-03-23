47122e018b5ad98491a00461551b333209ff1938
x44annie <kiyuuwithlove@gmail.com>
2026-03-23 18:20:04 +0100

    Initial commit

---
+++ src/main/java/com/data/priceguard/PriceguardApplication.java
@@ -0,0 +1,13 @@
+package com.data.priceguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceguardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/data/priceguard/PriceguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.priceguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceguardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
